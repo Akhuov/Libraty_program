80c6e17bc4b1ac54d13f21ead33451c37a08c3b5
user <user@WIN-F7NIMF7A3VO>
2023-06-19 18:07:26 +0500

    Добавьте файлы проекта.

---
+++ Libraty_prog/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Libraty_prog
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string Deseription { get; set; }
+        public Guid? Reader { get; set; }
+        public Book(string name, string description, string author)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Deseription = description;
+            Author = author;
+        }
+    }
+}
---
+++ Libraty_prog/LibraryManagment.cs
@@ -0,0 +1,28 @@
+﻿namespace Libraty_prog
+{
+    public abstract class LibraryManagmentCore
+    {
+        public LibraryStore Store { get; set; }
+
+        public LibraryManagmentCore()
+        {
+            Store = new LibraryStore();
+        }
+
+        public LibraryManagmentCore(List<Book> books, List<User> users)
+        {
+            Store = new LibraryStore(books, users);
+        }
+        public void DisplayAllUsers()
+        {
+            foreach (var user in Store.Users)
+                Console.WriteLine(user);
+        }
+
+        public void DisplayAllBooks()
+        {
+            foreach (var book in Store.Books)
+                Console.WriteLine(book);
+        }
+    }
+}
