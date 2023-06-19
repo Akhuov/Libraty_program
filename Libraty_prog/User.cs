6d03e00d7700a17e5250294502ba16aee11eb5a2
user <user@WIN-F7NIMF7A3VO>
2023-06-19 18:33:52 +0500

    Ubdate dataFunc

--- Libraty_prog/User.cs
+++ Libraty_prog/User.cs
@@ -1,9 +1,10 @@
-﻿namespace Libraty_prog
+﻿using System.Xml.Linq;
+
+namespace LibraryManagement
 {
     public class User
     {
         public Guid Id { get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
@@ -18,6 +19,15 @@
         public User(string firstName, string lastName, string userName) : this(firstName, lastName)
         {
             UserName = userName;
+        }
+
+        public override string ToString()
+        {
+            return $"Foydalanuvchi" +
+            $"\n id : {Id}, " +
+                $"\n ismi : {FirstName}, " +
+                $"\n familyasi : {LastName}" +
+                $"\n sharifi : {UserName}\n";
         }
     }
 }
